f611b0813eb15bd1f2d57ad24faac3eba92130a1
edupoly <95899099+edupoly@users.noreply.github.com>
2025-01-10 22:42:49 +0530

    Update leadsApi.js

--- src/services/leadsApi.js
+++ src/services/leadsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const leadsApi = createApi({
   reducerPath: 'leadsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:7777' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://edupoly-crm-backend-production.up.railway.app' }),
   endpoints: (builder) => ({
     getLogin: builder.query({
       query: () => `/`,
